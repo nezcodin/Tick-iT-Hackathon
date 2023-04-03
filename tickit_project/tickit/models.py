8a740f7c79cfeb44079795fea066b00224d3a6f6
meixingc <realmeixing@gmail.com>
2023-04-03 19:43:35 -0400

    reverted and changed models

--- tickit_project/tickit/models.py
+++ tickit_project/tickit/models.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.conf import settings
 
 class Venue(models.Model): 
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='member')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='member', null=True)
     name = models.CharField(max_length=100)
     location = models.TextField(default='location')
-
     def __str__(self):
         return self.name
 
