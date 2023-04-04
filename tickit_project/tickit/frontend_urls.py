73ddcc3067e60b5f9e6e9a2e07817dfd28d65cea
Duncan <duncanwoodpro@gmail.com>
2023-04-04 11:04:51 -0400

    Getting CRUD done!

--- tickit_project/tickit/frontend_urls.py
+++ tickit_project/tickit/frontend_urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from . import views
+from .views import VenueCreateView, VenueUpdateView, VenueDeleteView
+
 
 urlpatterns = [
     path('', views.landing_view, name='home'),
     path('events/<int:pk>/', views.event_details_view, name='event_details_view'),
     path('venues/<int:pk>/', views.venue_details_view, name='venue_details_view'),
-    path('events/', views.events_view, name='events'),
+    path('events/', views.events_view, name='events_list_view'),
     path('register/', views.registration, name='register'),
 
+    path('create/', VenueCreateView.as_view(), name='venue_create_view'),
+    path('venues/<int:pk>/update/', VenueUpdateView.as_view(), name='venue_update_view'),
+    path('delete/', VenueDeleteView.as_view(), name='venue_delete_view')
 ]
-
