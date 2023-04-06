2f89a1fa37a3e11d5ba34258554cbb7777400042
nezcodin <ch.fusco104@gmail.com>
2023-04-06 02:36:16 -0500

    venues model has description field

--- tickit_project/tickit_django/settings.py
+++ tickit_project/tickit_django/settings.py
@@ -102,10 +102,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
